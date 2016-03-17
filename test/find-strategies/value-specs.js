f1a071b1ba224e1fcd5d599186865954f3e8c32d
Dan Gilkerson <contact@dangilkerson.com>
2016-03-17 13:14:17 -0700

    Search by value

---
+++ test/find-strategies/value-specs.js
@@ -0,0 +1,18 @@
+import dom from "../dom"
+import findByValue from '../../src/find-strategies/value';
+
+describe("Find strategy: Value Match", function() {
+    beforeEach(function(){
+        document.body.innerHTML = "";
+    });
+
+    it("should find in value", function() {
+        var input = dom.create("input", "", {value: "enter name"});
+        findByValue("enter name", document).should.deep.equal([input]);
+    });
+
+    it("should find in value case insensitive", function() {
+        var input = dom.create("input", "", {vAlUe: "eNter namE"});
+        findByValue("enteR naMe", document).should.deep.equal([input]);
+    });
+});
