1a4345f4d62b65674d64ea993be710de70cf7ba5
Dan Gilkerson <contact@dangilkerson.com>
2016-08-19 11:52:50 -0700

    If an element doesn’t pass the simple offsetParent check for visibility, get it’s computed style.

--- src/extensions/visible.js
+++ src/extensions/visible.js
@@ -3,7 +3,14 @@ export default {
         "visible": {
             filter: function visible({elements}, resultHandler) {
                 return browserExecute(function (elements, handler) {
-                    return handler(null, elements.filter(e => e.tagName.toLowerCase() == "option" || e.offsetParent))
+                    return handler(null, elements.filter(e => {
+                        if(e.tagName.toLowerCase() == "option" || e.offsetParent)
+                            return true;
+                        else {
+                            var style = window.getComputedStyle(e);
+                            return (style.display != 'none');
+                        }
+                    }));
                 }, elements, resultHandler);
             }
         }
