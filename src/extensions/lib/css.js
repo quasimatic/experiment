5d7844f47d63568c26389868c1f51f3074ea5711
Dan Gilkerson <contact@dangilkerson.com>
2016-08-19 11:52:19 -0700

    Better error handling around processing invalid css.

--- src/extensions/lib/css.js
+++ src/extensions/lib/css.js
@@ -1,16 +1,19 @@
 export default function (label, scopeElement, resultHandler = (err, result) => result) {
-    try {
-        return browserExecute(function (label, scopeElement, handler) {
+    return browserExecute(function (label, scopeElement, handler) {
+        try {
             var results = scopeElement.querySelectorAll(label);
             return handler(null, Array.prototype.slice.apply(results));
-        }, label, scopeElement, (err, result)=> {
-            if (err)
-                return resultHandler(err, []);
+        }
+        catch(e) {
+            if(e instanceof DOMException)
+                return handler(null, []);
+            else
+                return handler(e, []);
+        }
+    }, label, scopeElement, (err, result)=> {
+        if (err)
+            return resultHandler(err, []);
 
-            return resultHandler(err, result);
-        });
-    }
-    catch (err) {
-        return resultHandler(err, []);
-    }
+        return resultHandler(err, result);
+    });
 }
