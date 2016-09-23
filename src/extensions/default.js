a835e2938f51f58a145cfa5fb16ae6b59e64cf7a
Dan Gilkerson <contact@dangilkerson.com>
2016-09-23 12:07:45 -0700

    Add search in type to defaults

--- src/extensions/default.js
+++ src/extensions/default.js
@@ -6,10 +6,11 @@ import searchAsId from './search-as-id';
 import searchAsClassName from './search-as-class-name';
 import searchInName from './search-in-name';
 import searchInAlt from './search-in-alt';
+import searchInType from './search-in-type';
 import searchAsNodeType from './search-as-nodetype';
 import searchAsCss from './search-as-css';
 import visible from './visible';
 import inputafter from './inputafter';
 import shortestscopeandcontainerpath from './shortest-scope-and-container-path';
 
-export default [searchInText,searchExactText,searchInValue,searchInPlaceholder,searchAsId,searchAsClassName,searchInName,searchInAlt,searchAsNodeType,searchAsCss,visible,inputafter,shortestscopeandcontainerpath];
+export default [searchInText,searchExactText,searchInValue,searchInPlaceholder,searchAsId,searchAsClassName,searchInName,searchInAlt,searchInType,searchAsNodeType,searchAsCss,visible,inputafter,shortestscopeandcontainerpath];
