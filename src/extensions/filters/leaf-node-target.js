cdc7cf4b58d1f46e77397fc9fb28893e1cd7db6b
Dan Gilkerson <contact@dangilkerson.com>
2017-01-17 22:14:34 -0800

    Refactor leaf node target spec

--- src/extensions/filters/leaf-node-target.js
+++ src/extensions/filters/leaf-node-target.js
@@ -1,33 +1,29 @@
-import log from "../../log"
+import log from '../../log'
+import defaultHandler from '../../utils/default-result-handler'
 import browserExecute from '../../browser-execute'
 
 export default {
     options: {
         "leaf-node-target": {
-            filter: function visible({target, elements} , resultHandler) {
+            filter: function visible({target, elements}, resultHandler = defaultHandler) {
                 log.debug("Filtering for leaf node targets");
 
                 if (target.type != "target") return resultHandler(null, elements);
 
                 return browserExecute(function (elements, handler) {
-                    try {
-                        var filteredElements = elements.filter(function (e) {
-                            return !e.childNodes
-                                || e.childNodes.length == 0
-                                || [].slice.call(e.childNodes).every(function (c) {
-                                    return c.nodeType == Node.TEXT_NODE
-                                })
-                        });
+                    var filteredElements = elements.filter(function (e) {
+                        return !e.childNodes
+                            || e.childNodes.length == 0
+                            || [].slice.call(e.childNodes).every(function (c) {
+                                return c.nodeType == Node.TEXT_NODE
+                            })
+                    });
 
-                        if(filteredElements.length == 0) {
-                            return handler(null, elements);
-                        }
+                    if (filteredElements.length == 0) {
+                        return handler(null, elements);
+                    }
 
-                        return handler(null, filteredElements);
-                    }
-                    catch (err) {
-                        return handler(err, []);
-                    }
+                    return handler(null, filteredElements);
                 }, elements, resultHandler);
             }
         }
