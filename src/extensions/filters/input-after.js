037483afaa120e4622a997d11e5e9bfc794363bc
Dan Gilkerson <contact@dangilkerson.com>
2017-01-17 22:06:35 -0800

    Refactor input after specs

--- src/extensions/filters/input-after.js
+++ src/extensions/filters/input-after.js
@@ -1,20 +1,22 @@
 import log from "../../log"
 import browserExecute from '../../browser-execute'
+import defaultHandler from '../../utils/default-result-handler';
 
 export default {
     options: {
         "input-after": {
-            filter: function inputafter({elements, scopeElements}, resultHandler) {
+            filter: function inputafter({elements, scopeElements}, resultHandler = defaultHandler) {
                 log.debug("Filtering for sibling input next to scope");
 
-                return browserExecute(function (elements, scopeElements, handler) {
-                    let siblings = elements.filter(function (e) {
+                return browserExecute(function (eee, scopeElements, handler) {
+                    let siblings = eee.filter(function (e) {
                         if (e.nodeName.toLowerCase() == "input") {
                             return e.previousElementSibling && e.previousElementSibling.nodeName.toLowerCase() != "input" && scopeElements.indexOf(e.previousElementSibling) != -1;
                         }
 
                         return false;
                     });
+
                     return handler(null, siblings.length == 0 ? elements : siblings);
                 }, elements, scopeElements, resultHandler);
             }
