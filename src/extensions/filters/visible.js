73a2b737a8d418284cc0062c4226037ab42245aa
Dan Gilkerson <contact@dangilkerson.com>
2017-01-18 07:27:25 -0800

    Refactor visible spec

--- src/extensions/filters/visible.js
+++ src/extensions/filters/visible.js
@@ -1,27 +1,23 @@
 import log from "../../log"
 import browserExecute from '../../browser-execute'
+import defaultHandler from '../../utils/default-result-handler'
 
 export default {
     options: {
         "visible": {
-            filter: function visible({elements}, resultHandler) {
+            filter: function visible({elements}, resultHandler = defaultHandler) {
                 log.debug("Filtering for visible elements");
 
                 return browserExecute(function (elements, handler) {
-                    try {
-                        return handler(null, elements.filter(function(e)  {
-                            if (e.tagName.toLowerCase() == "option" || e.offsetParent) {
-                                return true;
-                            }
-                            else {
-                                var style = window.getComputedStyle(e);
-                                return style.position == 'fixed' && style.display != 'none' && style.visibility != 'hidden';
-                            }
-                        }));
-                    }
-                    catch (err) {
-                        return handler(err, []);
-                    }
+                    return handler(null, elements.filter(function (e) {
+                        if (e.tagName.toLowerCase() == "option" || e.offsetParent) {
+                            return true;
+                        }
+                        else {
+                            var style = window.getComputedStyle(e);
+                            return style.position == 'fixed' && style.display != 'none' && style.visibility != 'hidden';
+                        }
+                    }));
                 }, elements, resultHandler);
             }
         }
